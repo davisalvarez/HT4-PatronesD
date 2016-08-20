07326cc48260f077bb8c939c9dc2e6134edd1ba2
DavisAlvarez <DavisAlvarez@192.168.1.6>
2016-08-19 22:02:56 -0600

    Creación

---
+++ H4-PatronesD/src/org/uvg/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Autores:
+ *		Jennifer Barillas -15307
+ *		Davis Alvarez - 15842
+ */
+
+package org.uvg.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
